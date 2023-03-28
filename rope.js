1d445b842dd45de20461476ab7597ab91abecf48
Alfie1400 <116584540+Alfie1400@users.noreply.github.com>
2023-03-28 16:49:53 +0100

    Update rope.js

--- rope.js
+++ rope.js
@@ -4,7 +4,7 @@ class Rope
     {
       this.nlink = nlink
   const group = Body.nextGroup(true);
-  const rects = Composites.stack(100, 100, this.nlink, 1, 5, 5, function(x, y) {
+  const rects = Composites.stack(150, 150, this.nlink, 1, 5, 5, function(x, y) {
       return Bodies.rectangle(x, y, 30, 5, { collisionFilter: { group: group } });
   });
       
